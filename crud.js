eaf5097b6f95436f1b182a22a0d322575f7eaba2
CodyG <gallagher2021@gmail.com>
2021-05-09 11:50:25 -0600

    Added in an update role option. Need to make it more secure

--- crud.js
+++ crud.js
@@ -20,8 +20,10 @@ function init() {
 			message: 'What would you like to do?',
 			choices: [
 				'View all Employees.',
+				'View Salaries.',
 				'View all Departments.',
 				'View all Roles.',
+				'Update Employee Role.',
 				'Add Department, Role, or Employee.',
 				'Exit'
 			],
@@ -30,6 +32,9 @@ function init() {
 			switch (res.choice) {
 				case 'View all Employees.':
 					employeeSearch();
+					break;
+				case 'View Salaries.':
+					employeeSalaries();
 					break;
 				case 'Add Department, Role, or Employee.':
 					addOption();
@@ -40,6 +45,9 @@ function init() {
 				case 'View all Roles.':
 					showRoles();
 					break;
+				case 'Update Employee Role.':
+					updateRole();
+					break;
 				default:
 					console.log('Thanks for using the app!');
 					connection.end();
@@ -48,7 +56,6 @@ function init() {
 		});
 };
 
-//Pulls the employee their title and role.
 function employeeSearch() {
 	connection.query(
 		`SELECT 
@@ -61,6 +68,21 @@ function employeeSearch() {
 		(err, res) => {
 			if (err) throw err;
 			console.table("-Employees-", res);
+			init();
+		}
+	);
+};
+
+function employeeSalaries() {
+	connection.query(
+		`SELECT 
+		CONCAT(e.first_name, ' ', e.last_name) AS 'Name',
+		r.salary AS 'Salary'
+		FROM employee e
+		LEFT JOIN role r ON e.role_id = r.id`,
+		(err, res) => {
+			if (err) throw err;
+			console.table("-Results-", res);
 			init();
 		}
 	);
@@ -88,7 +110,58 @@ function showRoles() {
 	);
 };
 
-function addOption () {
+function updateRole() {
+
+	connection.query(
+		`SELECT
+		e.id AS 'ID',
+		CONCAT(e.first_name, ' ', e.last_name) AS 'Name',
+		r.title AS 'Role'
+		FROM employee e
+		LEFT JOIN role r ON e.role_id = r.id
+		ORDER BY e.id`,
+		(err, res) => {
+			if (err) throw err;
+			console.table("-Current Employee Roles-", res);
+		}
+	);
+
+	connection.query(
+		`SELECT id AS 'ID', title AS 'ROLE' FROM role ORDER BY id`,
+		(err, res) => {
+			if (err) throw err;
+			console.table("-Available Roles-", res);
+		}
+	);
+	setTimeout(function(){
+		inquirer
+		.prompt([
+			{
+			name: 'updateID',
+			type: 'input',
+			message: 'Select the ID of the employee you want to update.'
+		},
+		{
+			name: 'roleID',
+			type: 'input',
+			message: 'Select the ID of the role you want.'
+		}
+		]).then((res) =>{
+			connection.query(
+				`UPDATE employee
+				SET role_id = ${res.roleID} 
+				WHERE id = ${res.updateID} `,
+				(err, res) => {
+					if (err) throw err;
+					console.log("Role Updated");
+					init();
+				})
+		}); 
+	}, 1000);
+
+};
+
+function addOption() {
 	inquirer
 	.prompt([
 		{
@@ -135,27 +208,26 @@ function addDepartment() {
 		)
 	})
 };
-
-// function addDepartment() {
-// 	inquirer
-// 	.prompt([
-// 		{
-// 			name: 'dept',
-// 			type: 'input',
-// 			message: 'What department would you like to add?'
-// 		}
-// 	]).then((res) => {
-// 		connection.query(
-// 			`INSERT INTO department (name)
-// 			VALUES ('${res.dept}')`,
-// 			(err, res) => {
-// 				if (err) throw err;
-// 				console.table('Department Added.');
-// 				init();
-// 			}
-// 		)
-// 	})
-// };
+function addRole() {
+	inquirer
+	.prompt([
+		{
+			name: 'dept',
+			type: 'input',
+			message: 'What role would you like to add?'
+		}
+	]).then((res) => {
+		connection.query(
+			`INSERT INTO department (name)
+			VALUES ('${res.dept}')`,
+			(err, res) => {
+				if (err) throw err;
+				console.table('Department Added.');
+				init();
+			}
+		)
+	})
+};
 
 connection.connect((err) => {
 	if (err) throw err;
